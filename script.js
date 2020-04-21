4941328b1ef9001327d6b6a322db9ae4e4107a77
Troy Hoffman <troy.hoffman@msan48f5cd16.attlocal.net>
2020-04-21 14:26:26 -0700

    Add exisiting Files

---
+++ script.js
@@ -0,0 +1,40 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function (event){
+  confirm ('Would you like to add Uppercase letters to password?');
+  confirm ('Would you like to add Lowercase letter?');
+  confirm ('Would you like to add Numbers?');
+  confirm ('Would you like to add special characters?');
+});
+
+
+
+// Generate Functions
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
